c27651499fae8192cf1d49072864e1d1d72c06ef
Steve Malers <steve.malers@openwaterfoundation.org>
2020-05-20 17:32:27 -0600

    Define map for DWR water districts, issue #6.

--- dist/copy-local-to-info-mapper.sh
+++ dist/copy-local-to-info-mapper.sh
@@ -7,5 +7,13 @@ scriptFolder=$(cd $(dirname "$0") && pwd)
 localFolder="${scriptFolder}/info-mapper-local"
 infoMapperFolder="${scriptFolder}/info-mapper"
 
-cp -v ${localFolder}/app-config.json ${infoMapperFolder}/app-config.json
-cp -rv ${localFolder}/content-pages ${infoMapperFolder}
+echo "Copy 'info-mapper-local/' files to 'info-mapper'."
+echo "Make sure that edits to app-config.json and content-pages were in 'info-mapper-local'."
+read -p "Are you sure [Y/n]? " answer
+if [ -z "${answer}" -o "${answer}" = "Y" -o "${answer}" = "y" ]; then
+  cp -v ${localFolder}/app-config.json ${infoMapperFolder}/app-config.json
+  cp -rv ${localFolder}/content-pages ${infoMapperFolder}
+  cp -rv ${localFolder}/content-pages/home.md ${infoMapperFolder}
+else
+  echo "Did not copy anything."
+fi
